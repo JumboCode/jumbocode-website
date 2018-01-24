2cf3333e92d1a3b432a08c7576678a1ea057fdfe
Peter Lam <Peter.Lam@Tufts.edu>
2018-01-23 20:37:48 -0500

    Updated JavaScript

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
 		}, 200)
 	});
 
+	if (! ($('.navigation').hasClass('open-nav'))) {
+		$("a").hover(function() {
+			$(this).animate({
+			    color: "#64DDBB"
+	  		}, 200 );
+		}, function() {
+			$(this).animate({
+				color: "white"
+			}, 200)
+		});
+	}
+	$("#ham").click(function(){
+    	$(".navigation").toggleClass("open-nav");
+	});
+
 	$('.overlay').hover(function() {
 		$(this).stop().animate({
 			opacity: 1
